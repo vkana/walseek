cab8a55cf05276d4822852e471a467e521844c8d
vkana <dearvenumadhav@gmail.com>
2018-05-01 00:29:21 -0500

    code cleanup

--- src/App.js
+++ src/App.js
@@ -3,80 +3,23 @@ import './App.css';
 //import logo from './logo.svg';
 const axios = require('axios');
 const stores = require('./stores.json');
-//===============
-const secrets = require('./secrets.json');
-const apiKey = secrets.apiKey;
-const walmart = require('walmart')(apiKey);
 
 const queryString = require('query-string');
 
 let allStores = stores.allStores;
-let failedStores = [];
 
 const randomApiDomain = () => {
-  let domains = ['walseek.herokuapp.com', 'walseek1.herokuapp.com', 'walseek2.herokuapp.com'];
+  let domains = ['walseek-rest.herokuapp.com', 'walseek-rest-1.herokuapp.com', 'walseek-rest-2.herokuapp.com'];
   return domains[Math.floor(Math.random()*domains.length)];
 }
 const formatCurrency = (num) => {
   return '$' + Number.parseFloat(num).toFixed(2);
 }
 const saveSearch = (product) => {
-  let url = 'https://walseek.herokuapp.com/products';
+  let url = 'https://walseek-rest.herokuapp.com/products';
   //let url = 'http://localhost:3001/products';
   axios.post(url, product).catch(e => console.log('save search failed'));
 }
-
-// const getUPC = async (sku) => {
-//   const apiKey = secrets.apiKey;
-//   const url = `https://cors-anywhere.herokuapp.com/https://api.walmartlabs.com/v1/items/${sku}?apiKey=${apiKey}`;
-//   let resp = await axios.get(url);
-//   return resp.data.upc;
-// };
-
-const getPrice = (response => {
-  let priceObj = {
-    "no": 0,
-    "price": 9999,
-    "stock": "NA"
-  };
-  if (response && response.data && response.data.data && response.data.data.inStore) {
-    priceObj = {
-      "no": response.data.data.inStore.storeId,
-      "price": response.data.data.inStore.price.priceInCents / 100,
-      "stock": response.data.data.inStore.inventory ? response.data.data.inStore.inventory.status : 'NA'
-    };
-    //priceObj.stock = response.data.data.inStore.inventory ? response.data.data.inStore.inventory.status : 'NA';
-  }
-  return priceObj;
-});
-
-const productDetails = (result) => {
-  return {
-    name: result.data.data.common.name,
-    sku: result.data.data.common.productId.wwwItemId,
-    upc: result.data.data.common.productId.upca,
-    url: result.data.data.common.productUrl,
-    bsUrl: 'https://www.brickseek.com/walmart-inventory-checker?sku=' + result.data.data.common.productId.wwwItemId,
-    available: result.data.data.common.offerType
-  };
-}
-
-const getStore = storeId => {return allStores.filter(st => st.no === storeId)[0]};
-
-const getStorePricePromise = (upc, storeId) => {
-
-  let url = `https://search.mobile.walmart.com/v1/products-by-code/UPC/${upc}`;
-  return axios.get(url, {
-    params: {
-      storeId: storeId
-    }
-  }).catch(e => {
-    failedStores.push(storeId);
-    return e;
-  })
-};
-
-
 //===============
 class App extends Component {
   constructor() {
@@ -99,7 +42,7 @@ class App extends Component {
   }
 
   searchHistory = async () => {
-    let url = 'https://walseek.herokuapp.com/products?count=50';
+    let url = 'https://walseek-rest.herokuapp.com/products?count=50';
     //let url = 'http://localhost:3001/products';
     let searches = [];
     axios.get(url).then(response => {
@@ -158,6 +101,7 @@ class App extends Component {
           storePrices.unshift(s);
           [lowPrice, lowZip] = [s.price, s.zip];
         }
+        return null;
         });
 
         if (zip) {
