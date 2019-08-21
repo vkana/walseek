5ed5f88919a73a536717ee6aff418ae2e52ab5f7
Venu Kanaparthy <dearvenumadhav@gmail.com>
2019-08-20 21:46:27 -0500

    Updates

---
+++ precache-manifest.6ac2132022d3cc83503b32757c393cc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3b0e210550de5aab13c8500345a1fc",
+    "url": "/walseek/index.html"
+  },
+  {
+    "revision": "63c0f2d26f074471b572",
+    "url": "/walseek/static/css/main.63df5139.chunk.css"
+  },
+  {
+    "revision": "13e379a1e09facb4cc8d",
+    "url": "/walseek/static/js/2.599e05e4.chunk.js"
+  },
+  {
+    "revision": "63c0f2d26f074471b572",
+    "url": "/walseek/static/js/main.46e68a4a.chunk.js"
+  },
+  {
+    "revision": "81fef8d6e0b07a6029ed",
+    "url": "/walseek/static/js/runtime~main.994dfd8d.js"
+  }
+]);
